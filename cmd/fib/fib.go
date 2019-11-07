ac39964ed1f047289b8ab835f5278e6187ddb74f
Aaron <AaronBatilo@gmail.com>
2019-11-07 08:15:46 -0700

    I think this works

--- cmd/fib/fib.go
+++ cmd/fib/fib.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/abatilo/go-package-testing/internal/foo"
 	"github.com/abatilo/go-package-testing/pkg/bar"
 )
 
 func main() {
-	// fmt.Println(foo.Foo)
+	fmt.Println(foo.Foo)
 	fmt.Println(bar.Bar)
 }
